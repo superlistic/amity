79fcf6c22a5983c94820f730c581644b1bf1fd92
Jay <53336235+johan-STYL@users.noreply.github.com>
2021-01-09 12:23:20 +0100

    remove  meeting on logout

--- server/routes/logoutRouter.js
+++ server/routes/logoutRouter.js
@@ -4,11 +4,20 @@ const chalk = require('chalk');
 const Jogger = require('../Jogger');
 const log = new Jogger(chalk.magenta('[logoutRouter]'));
 
-const logoutRouter = () => {
+const logoutRouter = (meetings, io) => {
   const logout = (req, res) => {
     log.debug('logging out user.', req.token.userId);
     res.clearCookie('x-access-token');
     res.redirect(302, '/');
+
+    const socketId = meetings.socketId(req.token.userId);
+    meetings.remove(socketId);
+    const peerId = meetings.match(socket.userId);
+    io.to(meetings.socketId(peerId)).emit('friendDisconnected', {
+      msg: '[socket] partner disconnected',
+    });
+
+    log.info('disconnected', socket.id), log.mute(reason);
   };
 
   router.get('/', logout);
